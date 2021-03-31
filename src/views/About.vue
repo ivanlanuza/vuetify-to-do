bcab8eb4b72d682baec3d015f05dfc8f5e93d869
Ivan Lanuza <icdlanuza@gmail.com>
2021-03-31 08:00:09 +0800

    Fixed About Page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="about pa-6">
     <h2>About {{ $store.state.appTitle }}</h2>
-    <p>This simple To Do list is made using Vue.js.  Your data is protected and will not be saved in the cloud.  Your data will only stay in your local browser.  Web App works in mobile, tablet, and desktop devices</p>
-    <p>Full code repository can be found in: <a href="https://github.com/ivanlanuza/vuetify-to-do">https://github.com/ivanlanuza/vuetify-to-do</a></p>
+    <p>This simple To Do list is made using Vue.js.  
+      Your data is protected and will not be saved in the cloud.  
+      Your data will only stay in your local browser.  
+      This app works in mobile, tablet, and desktop devices
+    </p>
+    <p>Full code repository can be found in: 
+        <a href="https://github.com/ivanlanuza/vuetify-to-do" class="text-subtitle-2">
+        https://github.com/ivanlanuza/vuetify-to-do
+        </a>
+    </p>
     <BR/>
     <h4>Features Available:</h4>
-    <ul>
+    <ul class="text-body-2">
       <li>Responsive UI design</li>
       <li>Add Task</li>
       <li>Edit Task with Dialog</li>
@@ -23,7 +31,7 @@
     </ul>
     <BR/><BR/>
     <h4>Plug-ins Used:</h4>
-    <ul>
+    <ul class="text-body-2">
       <li>vuex for state management</li>
       <li>vue-router for easy routing</li>
       <li>vuetify for UI components</li>
@@ -32,14 +40,15 @@
       <li>localbase for persistent storage</li>
     </ul>
     <BR/><BR/>
-    <h4>Missing Features:</h4>
-    <ul>
+    <h4>Wishlist Features:</h4>
+    <ul class="text-body-2">
       <li>Password Protection</li>
       <li>Change Name/Avatar</li>
       <li>Sync to cloud back-end</li>
       <li>Hide completed tasks</li>
       <li>Add tags/projects to tasks</li>
       <li>Filter by Task Due Date</li>
+      <li>Week / Today View </li>
     </ul>
 
   </div>
--- src/views/NoAccess.vue
+++ src/views/NoAccess.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="Public pa-6">
     <h2>This is a Protected Page</h2><BR/>
-    <p>Sorry, you have reached a protected page.<BR/>  You have to be an administrator to access this areas.</p>
+    <p>Sorry, you have reached a protected page.<BR/>  You have to be an administrator to access this area.</p>
   </div>
 </template>
